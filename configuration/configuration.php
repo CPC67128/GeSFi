a33756813c0c49270942b2dbe6a23b17134df5c7
CPC67128 <steve.fuchs@free.fr>
2014-09-29 15:59:37 +0200

    Add some comments

--- configuration/configuration.php
+++ configuration/configuration.php
@@ -1,20 +1,46 @@
 <?php
+/* ================================
+ * BudgetFox configuration.php file
+ * ================================
+ */ 
+
+/* Database Connection Information:
+ * --------------------------------
+ * - DB_HOST: MySQL Database server name or IP
+ * - DB_NAME: Database name on the server
+ * - DB_USER: Database user
+ * - DB_PASSWORD: User's password
+ * - DB_TABLE_PREFIX: BudgetFox database tables prefix (for database hosting several web applications) 
+ */
+
 $DB_HOST = "localhost";
 $DB_NAME = "budgetfox";
 $DB_USER = "root";
 $DB_PASSWORD = "";
 
-$DB_TABLE_PREFIX = "bf_"; // Can be for example demo_ ...
+$DB_TABLE_PREFIX = "bf_";
 
-$READ_ONLY = false;
+/* Emailing Configuration:
+ * -----------------------
+ * - EMAIL_ACTIVE: true or false, indicates if the application is allowed to send emails
+ * - EMAIL_FROM: emails sender address
+ * - EMAIL_ADMINISTRATION: administrator's email 
+ */
 
 $EMAIL_ACTIVE = false;
 
 $EMAIL_FROM = "do-not-respond@nowhere.nowhere";
 $EMAIL_ADMINISTRATION = "contact@nowhere.nowhere";
 
+/* Global Security Configuration:
+ * ------------------------------
+ * - READ_ONLY: true or false, indicates if the application can insert/update/delete data in the database (this is mainly used for the demonstration mode)
+ * - SECURITY_SINGLE_USER_MODE: true or false
+ * If set to true, the following parameter must be set
+ * - SECURITY_SINGLE_USER_MODE_USER_ID: user id the application should automatically connect 
+ */
+
+$READ_ONLY = false;
+
 $SECURITY_SINGLE_USER_MODE = false;
-/* if $SECURITY_SINGLE_USER_MODE is set to true, the following parameters must be configured:
- * $SECURITY_SINGLE_USER_MODE_USER_ID = 'e9ee6ea8-393e-11e2-ad8e-00ff9f7445d4'; <-- fill with the correct user-id you want to be connected with
-*/
 $SECURITY_SINGLE_USER_MODE_USER_ID = '4768b151-bd52-11e2-8d63-5c260a87ddbb';

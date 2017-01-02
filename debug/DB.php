bb79d4c866048c9be9fddee950ebe9653ffaa59c
CPC67128 <steve.fuchs@free.fr>
2017-01-03 00:06:26 +0100

    debug

--- debug/DB.php
+++ debug/DB.php
@@ -8,12 +8,23 @@ include '../configuration/configuration.php';
 $dns = 'mysql:host=' . $DB_HOST . ';dbname=' . $DB_NAME;
 echo 'DNS = '.$dns.'<br />';
 $utilisateur = $DB_USER;
+echo 'User = '.$utilisateur.'<br />';
 $motDePasse = $DB_PASSWORD;
-$this->_connection = new PDO( $dns, $utilisateur, $motDePasse );
-$this->_connection->exec("SET CHARACTER SET utf8");
-$this->_dbTablePrefix = $DB_TABLE_PREFIX;
+echo 'Mdp = '.$motDePasse.'<br />';
 
-$this->_isReadOnly = $READ_ONLY;
+try
+{
+	$this->_connection = new PDO( $dns, $utilisateur, $motDePasse );
+	echo '.<br />';
+	$this->_connection->exec("SET CHARACTER SET utf8");
+	echo '.<br />';
+	$this->_dbTablePrefix = $DB_TABLE_PREFIX;
+	echo '.<br />';
+}
+catch ( Exception $e )
+{
+	echo "Connection à MySQL impossible : ", $e->getMessage();
+}
 
 echo 'Connected<br />';
 

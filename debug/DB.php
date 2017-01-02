592bc0962c6dced01e38984a15dfcfd8e0d1065d
CPC67128 <steve.fuchs@free.fr>
2017-01-03 00:01:06 +0100

    Debug

--- debug/DB.php
+++ debug/DB.php
@@ -2,6 +2,7 @@
 include '../configuration/configuration.php';
 
 $dns = 'mysql:host=' . $DB_HOST . ';dbname=' . $DB_NAME;
+echo 'DNS = '.$dns.'<br />';
 $utilisateur = $DB_USER;
 $motDePasse = $DB_PASSWORD;
 $this->_connection = new PDO( $dns, $utilisateur, $motDePasse );

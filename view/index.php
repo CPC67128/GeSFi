2576eb9b8bd23328018ea6159e804dd3a1558b54
CPC67128 <steve.fuchs@free.fr>
2017-08-26 14:36:22 +0200

    Removal of mobile version remaining code

--- view/index.php
+++ view/index.php
@@ -1,14 +1,39 @@
 <?php
-include '../configuration/configuration.php';
-require_once('../3rd_party/mobile_device_detect/mobile_device_detect.php');
-$mobile = mobile_device_detect();
+include '../component/component_autoload.php';
+include '../component/component_security.php';
+$translator = new Translator();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?= $translator->getTranslation("GeSFi : Logiciel de comptabilité personnelle") ?></title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="Description" content="Application en ligne gratuite de gestion financière du couple (compatibilité de couple ou comptabilité commune) écrite par Steve Fuchs">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link type="text/css" href="gesfi.css" rel="stylesheet" />
 
-$qry = $_SERVER['QUERY_STRING'];
-if($mobile == true)
-{
-	header("location:index_pc.php?$qry");
-}
-else
-{
-	header("location:index_pc.php?$qry");
-}
+<link rel="stylesheet" href="../3rd_party/jquery-ui-1.11.4/jquery-ui.min.css">
+<script src="../3rd_party/jquery-2.2.4.min.js"></script>
+<script src="../3rd_party/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+
+<script src="../3rd_party/highcharts-4.2.5/js/highcharts.js"></script>
+<script src="../3rd_party/highcharts-4.2.5/js/modules/exporting.js"></script>
+
+<script type="text/javascript" src="gesfi.js"></script>
+<script type="text/javascript" src="gesfi_operations.js"></script>
+</head>
+<body>
+<div id='topMenu' class='topMenu'>
+</div>
+<div id='topSecondLineMenu' class='topSecondLineMenu'>
+</div>
+<div id='leftMenu' class='leftMenu' align='center'>
+</div>
+
+<!-- ----------------------------------- Records display ----------------------------------- -->
+
+<div id='content' class='content'>
+<div id="records"></div>
+</div>
+</body>
+</html>

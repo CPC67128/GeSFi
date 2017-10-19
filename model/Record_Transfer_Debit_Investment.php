ec19a6bd2ce0f787213b27cf9c036897331a7a52
CPC67128 <steve.fuchs@free.fr>
2017-10-19 23:35:55 +0200

    Fix

--- model/Record_Transfer_Debit_Investment.php
+++ model/Record_Transfer_Debit_Investment.php
@@ -1,9 +1,9 @@
 <?php
 class Record_Transfer_Debit_Investment extends Record_Transfer_Debit
 {
-	public function __construct($accountId, $userId, $recordDate, $withdrawal, $designation, $recordGroupId)
+	public function __construct($accountId, $userId, $recordDate, $withdrawal, $designation, $confirmed, $recordGroupId)
 	{
-		parent::__construct($accountId, $userId, $recordDate, null, $designation, $recordGroupId);
+		parent::__construct($accountId, $userId, $recordDate, null, $designation, $confirmed, $recordGroupId);
 		$this->withdrawal = $withdrawal;
 	}
 }

57fafbde748b5e417cb9e30ab4f888cfd7c32d08
CPC67128 <steve.fuchs@free.fr>
2020-10-22 23:25:04 +0200

    Prepare for code push

--- setup/scripts/upgrade.024.sql
+++ setup/scripts/upgrade.024.sql
@@ -1,7 +1,5 @@
 ALTER TABLE `{TABLEPREFIX}account` ADD COLUMN `no_yield_display` TINYINT(1) NOT NULL DEFAULT '0' AFTER `no_color_in_dashboard`;
 
-ALTER TABLE `bf_account`
-	CHANGE COLUMN `availability_date` `availability_date` DATE NULL AFTER `creation_date`;
+ALTER TABLE `{TABLEPREFIX}account` CHANGE COLUMN `availability_date` `availability_date` DATE NULL AFTER `creation_date`;
 
-    ALTER TABLE `bf_account`
-	CHANGE COLUMN `closing_date` `closing_date` DATE NULL AFTER `availability_date`;
+ALTER TABLE `{TABLEPREFIX}account` CHANGE COLUMN `closing_date` `closing_date` DATE NULL AFTER `availability_date`;
